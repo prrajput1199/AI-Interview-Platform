7c4641dc512f7eda539dda039f516ab76f01a282
pratik <pratikrajput1199@gmail.com>
2026-08-13 16:20:05 +0530

    Updated react-query and main.tsx

--- ai-interview-frontend/ai-interview-client/src/main.tsx
+++ ai-interview-frontend/ai-interview-client/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { Provider } from 'react-redux'
+import { store } from './store/index.ts'
+import { queryClient } from './lib/react-query.ts';
+import './styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+       <QueryClientProvider client = {queryClient}>
+           <App/>
+       </QueryClientProvider>
+    </Provider>
   </StrictMode>,
 )
