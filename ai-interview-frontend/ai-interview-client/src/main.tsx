a919a97ddb16b1f3a5eaa813024ee2db7354d920
pratik <pratikrajput1199@gmail.com>
2026-08-17 17:12:09 +0530

    Added Sample home page

--- ai-interview-frontend/ai-interview-client/src/main.tsx
+++ ai-interview-frontend/ai-interview-client/src/main.tsx
@@ -1,18 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 import { store } from './store/index.ts'
 import { queryClient } from './lib/react-query.ts';
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes.tsx'
 // import './styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
        <QueryClientProvider client = {queryClient}>
-           <App/>
+           <RouterProvider router ={router}/>
        </QueryClientProvider>
     </Provider>
   </StrictMode>,
