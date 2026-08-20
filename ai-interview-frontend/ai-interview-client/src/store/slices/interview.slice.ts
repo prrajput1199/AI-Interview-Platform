eb6ef0d2ecc6642444b3c095498c8436de1d1493
pratik <pratikrajput1199@gmail.com>
2026-08-20 19:36:48 +0530

    Updated interview slice

--- ai-interview-frontend/ai-interview-client/src/store/slices/interview.slice.ts
+++ ai-interview-frontend/ai-interview-client/src/store/slices/interview.slice.ts
@@ -1,5 +1,23 @@
 import axiosInstance from "@/utils/axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+interface InterviewState {
+ currentInterview: any | null;
+ questions: any[];
+ currentQuestionIndex: number;
+ answers: any[];
+ isLoading: boolean;
+ error: string | null;
+}
+
+const initialState: InterviewState = {
+    currentInterview: null,
+    questions: [],
+    currentQuestionIndex: 0,
+    answers: [],
+    isLoading: false,
+    error: null
+}
 
 export const createInterview = createAsyncThunk(
     'interview/create',
@@ -25,3 +43,43 @@ export const fetchInterview= createAsyncThunk(
     }
 );
 
+
+const interviewSlice = createSlice({
+    name:'interview',
+    initialState,
+    reducers: {
+        setCurrentQuestion: (state,action) => {
+            state.currentQuestionIndex = action.payload
+        },
+        clearInterview: (state) => {
+            state.currentInterview = null;
+            state.questions = [];
+            state.currentQuestionIndex = 0;
+            state.answers =[]
+        },
+        addAnswer: (state,action) => {
+            state.answers.push(action.payload)
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+          .addCase(createInterview.pending, (state)=>{
+            state.isLoading = true;
+          })
+          .addCase(createInterview.fulfilled,(state,action) => {
+             state.isLoading = false;
+             state.currentInterview = action.payload;
+          })
+          .addCase(createInterview.rejected,(state,action)=>{
+               state.isLoading= false;
+               state.error = action.error.message || 'Failed tpo create interview'
+          })
+          .addCase(generateQuestions.fulfilled,(state,action) => {
+              state.questions = action.payload;
+          })
+          .addCase(fetchInterview.fulfilled,(state,action) => {
+              state.currentInterview = action.payload;
+              state.questions = action.payload.questions || []
+          })
+    }
+})
