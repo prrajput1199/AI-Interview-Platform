98d8fc04ae180bf4b75abc537f5569c49e336323
pratik <pratikrajput1199@gmail.com>
2026-08-25 17:18:04 +0530

    Added Loading Skeleton for InterviewSession

--- ai-interview-frontend/ai-interview-client/src/routes.tsx
+++ ai-interview-frontend/ai-interview-client/src/routes.tsx
@@ -38,7 +38,7 @@ export const router = createBrowserRouter([
         )
     },
     {
-        path:'interview-setup',
+        path:'/interview-setup',
         element: (
             <ProtectedRoute>
                 <Layout>
